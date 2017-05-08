2f32d09062b48c008246071d46ff7fb98075cc98
François CHABROU <developer@stopgapjs.org>
2017-05-08 23:53:23 +0200

    Delete unnecessary Led Pin

--- SSD1206_demo/src/main.cpp
+++ SSD1206_demo/src/main.cpp
@@ -407,19 +407,10 @@ static void MX_GPIO_Init(void) {
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   // Configure GPIO pin Output Level
-  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-
-  // Configure GPIO pin Output Level
   HAL_GPIO_WritePin(GPIOA, CS_Pin, GPIO_PIN_RESET);
 
   // Configure GPIO pin Output Level
   HAL_GPIO_WritePin(GPIOB, DC_Pin|RST_Pin, GPIO_PIN_RESET);
-
-  // Configure GPIO pin : LED_Pin
-  GPIO_InitStruct.Pin = LED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
   // Configure GPIO pins : CS_Pin CLK_Pin MOSI_Pin
   GPIO_InitStruct.Pin = CS_Pin;
